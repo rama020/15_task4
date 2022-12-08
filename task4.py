ba09769a6cb66d8010ec71006eedb63dd2089253
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-08 14:41:17 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,22 @@
+'''
+DATE:08TH NOV 2022
+DAY: SUNDAY
+TOPIC: TASK
+AUTHOR: RAMA BHARGAVI
+'''
+n=-1
+while(n<20):
+     n+=2
+     if n>8:
+        break
+     print(n)#1 3 5 7
+#2nd method
+     n=0
+     while(n<10):
+        n+=1
+        if n==4:
+            break
+        print(n)#1 2 3
+
+
+    
